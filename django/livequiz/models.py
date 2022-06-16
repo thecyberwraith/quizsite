f011c36146b697bae3b154f69558cb8cebeb4d38
Michael Lugo <michaelrlugo@gmail.com>
2022-06-16 23:44:53 +0000

    Added LiveQuizModel with tests

--- django/livequiz/models.py
+++ django/livequiz/models.py
@@ -1,3 +1,111 @@
-from django.db import models
+from string import ascii_uppercase, digits
 
-# Create your models here.
+from django.utils.crypto import get_random_string
+from django.db import models, IntegrityError, DatabaseError
+
+from quiz.models import QuizModel
+
+SLUG_SIZE = 8
+ALLOWED_CHARS = ascii_uppercase + digits
+
+UNIQUE_RETRIES = 5
+
+
+def generate_random_slug():
+    return get_random_string(
+        length=SLUG_SIZE,
+        allowed_chars=ALLOWED_CHARS
+    )
+
+
+class LiveQuizModel(models.Model):
+    '''
+    Represents a quiz that is currently being hosted. Multiple host instances
+    can be hosting the same quiz. The primary interface should be the register_for_quiz
+    and unregister_for_quiz methods, which handle model creation and destruction. The
+    register method returns a copy of the objects.
+    '''
+    code = models.SlugField(
+        max_length=SLUG_SIZE,
+        db_index=True,
+        primary_key=True
+    )
+
+    quiz = models.ForeignKey(
+        to=QuizModel,
+        on_delete=models.DO_NOTHING,
+        null=False
+    )
+
+    registered_hosts = models.IntegerField(default=1)
+
+    start_time = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    last_view_command = models.CharField(
+        max_length=512,
+        null=True
+
+    )
+
+    last_view_data = models.CharField(
+        max_length=1024,
+        null=True
+    )
+
+    @staticmethod
+    def create_for_quiz(quiz_id: int):
+        '''Creates a live quiz for this particular quiz, and generates a unique code.'''
+        quiz = QuizModel.objects.get(id=quiz_id)
+        code = generate_random_slug()
+        for _ in range(UNIQUE_RETRIES):
+            try:
+                LiveQuizModel.objects.get(code=code)
+                code = generate_random_slug()
+            except LiveQuizModel.DoesNotExist:
+                return LiveQuizModel.objects.create(
+                    code=code,
+                    quiz=quiz
+                )
+
+        raise DatabaseError(
+            f'Failed to create unique code for LiveQuiz within {UNIQUE_RETRIES} tries.'
+        )
+
+    @staticmethod
+    def register_for_quiz(quiz_id: int):
+        '''
+        Signifies that a host is registering to manage this quiz. If a hosted quiz already exists,
+        we increment the registered hosts count. Otherwise, we create a new live quiz entry.
+
+        Returns the created/retrieved live quiz.
+        '''
+
+        try:
+            quiz = LiveQuizModel.objects.get(quiz__id=quiz_id)
+            quiz.registered_hosts += 1
+            quiz.save()
+            return quiz
+        except LiveQuizModel.DoesNotExist:
+            pass
+
+        return LiveQuizModel.create_for_quiz(quiz_id=quiz_id)
+
+    @staticmethod
+    def unregister_for_quiz(quiz_id: int):
+        '''
+        Signifies that a host is unregistering from managing the quiz. If no more hosts are
+        managing, then the live quiz entry is deleted.
+
+        Returns boolean - True if the quiz is no longer managed.
+        '''
+        quiz = LiveQuizModel.objects.get(quiz__id=quiz_id)
+        quiz.registered_hosts -= 1
+
+        if quiz.registered_hosts <= 0:
+            quiz.delete()
+            return True
+
+        quiz.save()
+        return False
--- django/livequiz/tests.py
+++ django/livequiz/tests.py
@@ -1,5 +1,94 @@
+from unittest.mock import patch
+
+from django.db import DatabaseError
 from django.test import TestCase
 from django.urls import reverse
+
+from . import models
+from quiz.models import QuizModel
+
+
+class TestLiveQuizModelCreateForQuizMethod(TestCase):
+    '''Isolates tests to the LiveQuizModel'''
+
+    def setUp(self) -> None:
+        self.quizA = QuizModel.objects.create(name='A Quiz')
+        self.quizB = QuizModel.objects.create(name='B Quiz')
+
+    def test_can_generate_unique_codes(self):
+        x = models.LiveQuizModel.create_for_quiz(self.quizA.id)
+        y = models.LiveQuizModel.create_for_quiz(self.quizA.id)
+
+        self.assertNotEqual(x.code, y.code)
+
+    @patch.object(models, 'generate_random_slug', return_value='ABCDEFG')
+    def test_same_code_cannot_be_shared_between_quizzes(self, _):
+        models.LiveQuizModel.create_for_quiz(self.quizA.id)
+
+        with self.assertRaises(DatabaseError):
+            models.LiveQuizModel.create_for_quiz(self.quizB.id)
+
+    @patch.object(models, 'generate_random_slug')
+    def test_keeps_trying_to_create(self, gen_function):
+        codes = ['ABC'] * (models.UNIQUE_RETRIES)
+        codes.append('CBA')
+
+        gen_function.side_effect = codes
+
+        models.LiveQuizModel.create_for_quiz(self.quizA.id)
+
+        self.assertEqual(
+            models.LiveQuizModel.create_for_quiz(self.quizA.id).code,
+            'CBA'
+        )
+
+
+class TestLiveQuizModelRegisterAndUnregister(TestCase):
+    def setUp(self):
+        self.quiz_id = QuizModel.objects.create(name='A quiz').id
+
+    def test_register_initially_creates(self):
+        self.assertEqual(models.LiveQuizModel.objects.count(), 0)
+
+        models.LiveQuizModel.register_for_quiz(self.quiz_id)
+
+        self.assertEqual(models.LiveQuizModel.objects.count(), 1)
+
+    def test_register_does_not_make_copies(self):
+        self.assertEqual(models.LiveQuizModel.objects.count(), 0)
+
+        results = [models.LiveQuizModel.register_for_quiz(
+            self.quiz_id) for _ in range(10)]
+
+        self.assertEqual(models.LiveQuizModel.objects.count(), 1)
+        for i in range(1, 10):
+            self.assertEqual(results[0].code, results[i].code)
+
+    def test_unregister_decrements_registered_hosts_count(self):
+        for _ in range(6):
+            quiz = models.LiveQuizModel.register_for_quiz(self.quiz_id)
+
+        for _ in range(4):
+            models.LiveQuizModel.unregister_for_quiz(self.quiz_id)
+
+        self.assertEqual(models.LiveQuizModel.objects.get(
+            code=quiz.code).registered_hosts, 2)
+
+    def test_unregister_returns_false_when_registered_hosts_not_zero(self):
+        models.LiveQuizModel.register_for_quiz(self.quiz_id)
+        models.LiveQuizModel.register_for_quiz(self.quiz_id)
+        self.assertFalse(
+            models.LiveQuizModel.unregister_for_quiz(self.quiz_id))
+
+    def test_unregister_returns_true_when_registered_hosts_is_zero(self):
+        models.LiveQuizModel.register_for_quiz(self.quiz_id)
+        self.assertTrue(models.LiveQuizModel.unregister_for_quiz(self.quiz_id))
+
+    def test_unregister_deletes_quiz_when_hosts_is_zero(self):
+        code = models.LiveQuizModel.register_for_quiz(self.quiz_id).code
+        models.LiveQuizModel.unregister_for_quiz(self.quiz_id)
+        with self.assertRaises(models.LiveQuizModel.DoesNotExist):
+            models.LiveQuizModel.objects.get(code=code)
 
 
 class PageBasedTest(TestCase):
