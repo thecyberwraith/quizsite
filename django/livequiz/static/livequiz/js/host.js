06393d9ac482238aabcfb7bc14ddc198b64dd099
Michael Lugo <michaelrlugo@gmail.com>
2022-06-22 02:29:06 +0000

    Started handling client side for live quizzes

--- django/livequiz/static/livequiz/js/host.js
+++ django/livequiz/static/livequiz/js/host.js
@@ -1,5 +1,8 @@
 import { LiveQuizWebsocket } from "./websocket.js"
 
+const contentDiv = document.getElementById('livequiz_content_div');
+const templateBoard = document.getElementById('template_livequiz_board');
+
 export function setup(quiz_code) {
     new LiveQuizWebsocket('/ws/live/host/' + quiz_code);
 }
--- django/livequiz/static/livequiz/js/websocket.js
+++ django/livequiz/static/livequiz/js/websocket.js
@@ -2,9 +2,14 @@ import { getWebsocketURLFromLocation } from "./util.js"
 
 export class LiveQuizWebsocket {
     constructor(relative_url) {
-        let url = getWebsocketURLFromLocation(relative_url);
-        this.socket = new WebSocket(url);
+        this.relative_url = relative_url;
+        this.socket = null;
+        this.establishConnection();
+    }
 
+    establishConnection() {
+        let url = getWebsocketURLFromLocation(this.relative_url);
+        this.socket = new WebSocket(url);
         this.socket.onopen = this.onSocketOpen;
         this.socket.onclose = this.onSocketClose;
         this.socket.onmessage = this.onSocketMessage;
@@ -15,37 +20,19 @@ export class LiveQuizWebsocket {
     }
 
     onSocketClose(e) {
-        console.log(e);
+        if (!e.wasClean) {
+            if (this.socket !== null) {
+                this.socket.close();
+            }
+            console.log('Attempting to reconnect in 5 seconds.');
+            setTimeout(this.establishConnection, 5000);
+        }
+        else {
+            console.info('Server intentionally closed connection.');
+        }
     }
 
     onSocketMessage(e) {
-        console.log(e);
-    }
-}
-
-function establish_connection() {
-    if (games_socket !== null) {
-        games_socket.close();
-    }
-
-    games_socket = new WebSocket(websocket_url);
-
-    games_socket.onmessage = function(e) {
-        if (e.data.error) {
-            console.error('Problem from the server:', e.error)
-        }
-        console.log('Message', e.data);
-    }
-    games_socket.onclose = function(e) {
-        if (!e.wasClean) {
-            console.warn('Whoops. Socket closed.', e);
-            setTimeout(establish_connection, 15000);
-            }
-        else {
-            console.info('Clean disconnection.');
-        }
-    }
-    games_socket.onopen = function(e) {
-        console.log('It is open.')
+        console.log('Message: ', e.data);
     }
 }
